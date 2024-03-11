508b41152c2c9ebf2ab3b553f2c05cd799a46b6c
pranavtr25 <pranavtr25@gmail.com>
2024-03-11 10:52:30 +0530

    70. Climbing Stairs

---
+++ leetcode-70.js
@@ -0,0 +1,14 @@
+var climbStairs = function (n) {
+    if (n == 1) return 1
+
+    let pr1 = 1
+    let pr2 = 1
+    let curr = 0
+    for (let i = 2; i <= n; i++) {
+        curr = pr2 + pr1
+        pr2 = pr1
+        pr1 = curr
+
+    }
+    return curr
+};
