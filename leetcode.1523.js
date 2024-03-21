e5e3f0020a3a7124cc67f03ed97b6132d3807231
pranavtr25 <pranavtr25@gmail.com>
2024-03-21 10:04:58 +0530

    1523. Count Odd Numbers in an Interval Range

---
+++ leetcode.1523.js
@@ -0,0 +1,13 @@
+var countOdds = function (low, high) {
+    if ((high - low) % 2 !== 0) {
+        return Math.ceil((high - low)/2)
+    } else {
+        if (low % 2 !== 0) {
+            const val = (high - low) / 2
+            return val + 1
+
+        } else {
+            return (high - low) / 2
+        }
+    }
+};
