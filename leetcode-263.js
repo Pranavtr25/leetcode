1c2f2b26122e759d1ccf8bbdc4cdd6d1044a7938
pranavtr25 <pranavtr25@gmail.com>
2024-07-04 09:56:39 +0530

    263. Ugly Number

---
+++ leetcode-263.js
@@ -0,0 +1,28 @@
+// 263. Ugly Number
+
+var isUgly = function(n) {
+    if(n <= 0){
+        return false
+    } 
+    while(n > 0){
+        let flag = 0
+        if(n % 5 === 0){
+            flag = 1;
+            n = n/5
+        }else if(n % 3 === 0) {
+            flag = 1;
+            n = n/3
+        }else if(n % 2 === 0){
+            flag = 1;
+            n = n/2
+        }
+        if(!flag){
+            break;
+        }
+    }
+    if(n === 1){
+        return true
+    } else{
+        return false
+    }
+};
