1d0ddfbcf5480c509bdee5ea7745046536ad415f
Pranavtr25 <147588999+Pranavtr25@users.noreply.github.com>
2024-07-26 15:32:12 +0530

    Time: 53 ms (70.97%) | Memory: 48.9 MB (65.90%) - LeetSync

--- 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.js
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.js
@@ -4,11 +4,10 @@
  */
 var maxLengthBetweenEqualCharacters = function (s) {
     let obj = {}
-    let max = 0
+    let max = -1
     let flag = 0
     for (let i = 0; i < s.length; i++) {
         if (obj[s[i]]) {
-            flag = 1
             if (max < (i - obj[s[i]])) {
                 max = i - obj[s[i]]
             }
@@ -16,9 +15,5 @@ var maxLengthBetweenEqualCharacters = function (s) {
             obj[s[i]] = i + 1
         }
     }
-    if (flag) {
-        return max;
-    } else {
-        return -1;
-    }
+    return max;
 };
