ce344099a488f37659614cb6152281df6bfffb38
Pranavtr25 <147588999+Pranavtr25@users.noreply.github.com>
2024-07-30 09:49:23 +0530

    Time: 62 ms (14.14%) | Memory: 49.7 MB (11.32%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+    let index = -1
+    for (let i = 0; i < haystack.length; i++) {
+        if (haystack[i] === needle[0]) {
+            let firstIndex = i;
+            let flag = 1
+            let k = i
+            for (let j = 0; j < needle.length; j++) {
+                if (haystack[k++] !== needle[j]) {
+                    flag = 0
+                    break;
+                }
+            }
+            if (flag) {
+                index = firstIndex
+                return index
+            }
+        }
+    }
+    return index;
+};
