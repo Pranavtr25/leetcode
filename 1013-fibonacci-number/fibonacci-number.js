53ca81a3c0a1ac914064aab532c69b293df9964c
Pranavtr25 <147588999+Pranavtr25@users.noreply.github.com>
2024-08-30 09:16:14 +0530

    Time: 48 ms (82.10%) | Memory: 48.7 MB (48.79%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    if(n==0) return 0
+    if(n==1) return 1
+
+    let pr1=1
+    let pr2=0
+    let curr=0
+    for(let i=2;i<=n;i++){
+        curr=pr2+pr1
+        pr2=pr1
+        pr1=curr
+
+    }
+    return curr
+    
+};
