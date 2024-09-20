1069fd2240adece784c3d96dfafaceece9844d94
Pranavtr25 <147588999+Pranavtr25@users.noreply.github.com>
2024-09-20 13:27:54 +0530

    Time: 60 ms (35.06%) | Memory: 50.8 MB (25.21%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
@@ -10,28 +10,25 @@
  * @param {number} n
  * @return {ListNode}
  */
-var removeNthFromEnd = function(head, n) {
+var removeNthFromEnd = function (head, n) {
     let curr1 = head;
     let count = 0;
-    while(curr1?.val !== undefined){
+    while (curr1?.val !== undefined) {
         count++;
         curr1 = curr1.next;
     }
     count++;
-    
     let removeCount = 0;
     let curr2 = head
     let node = new ListNode(0, head);
     let prev = node
-    console.log(JSON.stringify(node))
-    while(curr2?.val !== undefined){
+    while (curr2?.val !== undefined) {
         removeCount++;
-        if(count - n === removeCount){
+        if (count - n === removeCount) {
             prev.next = prev.next?.next
             return node.next;
         }
         curr2 = curr2.next
         prev = prev.next
     }
-
 };
