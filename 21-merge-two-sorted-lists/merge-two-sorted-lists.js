3575091b79fe0a30641823a0cbf99b51b1b36920
Pranavtr25 <147588999+Pranavtr25@users.noreply.github.com>
2024-09-21 10:40:26 +0530

    Time: 70 ms (17.98%) | Memory: 52.4 MB (8.11%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -15,68 +15,13 @@ var mergeTwoLists = function(list1, list2) {
     let curr = res;
     let curr1 = list1
     let curr2 = list2
-    // if(curr1?.val < curr2?.val){
-    //     while(curr1?.val !== undefined){
-    //         if(curr2.val !== undefined){
-    //             if(curr1?.val < curr2?.val){
-    //                 curr.next = new ListNode(curr1.val)
-    //                 curr.next.next = new ListNode(curr2.val)
-    //             }else{
-    //                 curr.next = new ListNode(curr2.val)
-    //                 curr.next.next = new ListNode(curr1.val)
-    //             }
-    //             curr = curr.next.next
-    //             curr1 = curr1.next
-    //             curr2 = curr2.next
-    //         }else{
-    //             curr.next = new ListNode(curr1.val)
-    //             curr = curr.next
-    //             curr1 = curr1.next
-    //         }
-    //     }
-    // }else{
-    //     while(curr2?.val !== undefined){
-    //         if(curr1?.val !== undefined){
-    //             if(curr2?.val < curr1?.val){
-    //                 curr.next = new ListNode(curr2.val)
-    //                 curr.next.next = new ListNode(curr1.val)
-    //             }else{
-    //                 curr.next = new ListNode(curr1.val)
-    //                 curr.next.next = new ListNode(curr2.val)
-    //             }
-    //             curr = curr.next.next
-    //             curr1 = curr1.next
-    //             curr2 = curr2.next
-    //         }else{
-    //             curr.next = new ListNode(curr2.val)
-    //             curr = curr.next
-    //             curr2 = curr2.next
-    //         }
-    //     }
-    // }
-    // while(curr1?.val !== undefined){
-    //     curr.next = new ListNode(curr1.val)
-    //     curr1 = curr1.next
-    //     curr = curr.next
-    // }
-    // while(curr2?.val !== undefined){
-    //     curr.next = new ListNode(curr2.val)
-    //     curr2 = curr2.next
-    //     curr = curr.next
-    // }
-    // console.log(JSON.stringify(res))
-    // return res.next
-
-
     while(curr1?.val !== undefined && curr2?.val !== undefined){
         if(curr1?.val < curr2?.val){
             curr.next = new ListNode(curr1.val)
-            // curr.next.next = new ListNode(curr2.val)
             curr1 = curr1.next
         }else{
             curr.next = new ListNode(curr2.val)
             curr2 = curr2.next
-            // curr.next.next = new ListNode(curr1.val)
         }
         curr = curr.next
     }
